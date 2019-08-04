cb36dbb65512581cbbac93b6180e1ab8597c2443
Matthew Chrobak <MatthewChrobak@hotmail.com>
2019-08-04 17:09:04 -0400

    Added Int as a shared data type

--- Source/Annex/Data/IntRect.cs
+++ Source/Annex/Data/IntRect.cs
@@ -2,41 +2,20 @@
 {
     public class IntRect
     {
-        public int Top { get; set; }
-        public int Left { get; set; }
-        public int Width { get; set; }
-        public int Height { get; set; }
+        public Int Top { get; set; }
+        public Int Left { get; set; }
+        public Int Width { get; set; }
+        public Int Height { get; set; }
 
-        public int Right {
-            get {
-                return this.Left + this.Width;
-            }
-            set {
-                this.Width = value - this.Left;
-            }
-        }
-        public int Bottom {
-            get {
-                return this.Top + this.Height;
-            }
-            set {
-                this.Height = value - this.Top;
-            }
-        }
-
-        public IntRect(int top, int left, int width, int height) {
+        public IntRect(Int top, Int left, Int width, Int height) {
             this.Top = top;
             this.Left = left;
             this.Width = width;
             this.Height = height;
         }
 
-        public IntRect() : this(0, 0, 0, 0) {
-
-        }
-
         public static implicit operator SFML.Graphics.IntRect(IntRect source) {
-            return new SFML.Graphics.IntRect(source.Left, source.Top, source.Width, source.Height);
+            return new SFML.Graphics.IntRect(source.Left.Value, source.Top.Value, source.Width.Value, source.Height.Value);
         }
     }
 }
