8a84f5734fb5568689a32b43353a58142b20e6a1
Matthew Chrobak <MatthewChrobak@users.noreply.github.com>
2020-08-26 16:49:04 -0400

    Moved exception message formatting

--- source/Annex/AssertionFailedException.cs
+++ source/Annex/AssertionFailedException.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using static Annex.Paths;
 
 namespace Annex
 {
     public class AssertionFailedException : Exception
     {
-        public AssertionFailedException(string? message) : base(message) {
+        private static string FormatExceptionMessage(string reason, int line, string callingMethod, string filePath) {
+            string relativeFilePath = filePath[SolutionFolder.Length..];
+            return $"Failure in {relativeFilePath} on line {line} in the function {callingMethod}: {reason}";
+        }
 
+        public AssertionFailedException(string reason, int line, string callingMethod, string filePath)
+            : base(FormatExceptionMessage(reason, line, callingMethod, filePath)) {
         }
     }
 }
--- source/Annex/Debug.cs
+++ source/Annex/Debug.cs
@@ -1,20 +1,14 @@
 ﻿using System.Diagnostics;
 using System.Runtime.CompilerServices;
-using static Annex.Paths;
 
 namespace Annex
 {
     public static class Debug
     {
-        private static string FormatAndLog(string reason, int line, string callingMethod, string filePath) {
-            string relativeFilePath = filePath[SolutionFolder.Length..];
-            return $"Failure in {relativeFilePath} on line {line} in the function {callingMethod}: {reason}";
-        }
-
         [Conditional("DEBUG")]
         public static void Assert(bool condition, string reason, [CallerLineNumber] int line = 0, [CallerMemberName] string callingMethod = "unknown", [CallerFilePath] string filePath = "unknown") {
             if (!condition) {
-                throw new AssertionFailedException(FormatAndLog(reason, line, callingMethod, filePath));
+                throw new AssertionFailedException(reason, line, callingMethod, filePath);
             }
         }
 
