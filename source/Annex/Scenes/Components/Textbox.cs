2b3555555c40fade6a86c4cd0fc46b5e5377bbd3
Matthew Chrobak <MatthewChrobak@users.noreply.github.com>
2020-10-07 18:08:11 -0400

    Made textbox a label, closes #34

--- source/Annex/Scenes/Components/Textbox.cs
+++ source/Annex/Scenes/Components/Textbox.cs
@@ -1,34 +1,10 @@
-﻿using Annex.Data.Shared;
-using Annex.Graphics;
-using Annex.Graphics.Contexts;
-using Annex.Graphics.Events;
+﻿using Annex.Graphics.Events;
 
 namespace Annex.Scenes.Components
 {
-    public class Textbox : Image
+    public class Textbox : Label
     {
-        protected readonly TextContext RenderText;
-        public readonly String Text;
-        public readonly String Font;
-
         public Textbox(string elementID = "") : base(elementID) {
-            this.Text = new String();
-            this.Font = new String();
-
-            this.RenderText = new TextContext(this.Text, this.Font) {
-                RenderPosition = this.Position,
-                UseUIView = true,
-                Alignment = new TextAlignment() {
-                    Size = this.Size,
-                    HorizontalAlignment = HorizontalAlignment.Left,
-                    VerticalAlignment = VerticalAlignment.Middle
-                }
-            };
-        }
-
-        public override void Draw(ICanvas canvas) {
-            base.Draw(canvas);
-            canvas.Draw(this.RenderText);
         }
 
         public override void HandleKeyboardKeyPressed(KeyboardKeyPressedEvent e) {
