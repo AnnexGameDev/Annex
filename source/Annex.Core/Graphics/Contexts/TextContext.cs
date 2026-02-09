6693430f9699f0f63689458c616266e8bc113a2d
Matthew Chrobak <matthewchrobak@hotmail.com>
2026-02-08 23:53:25 -0500

    File scoped namespace

--- source/Annex.Core/Graphics/Contexts/TextContext.cs
+++ source/Annex.Core/Graphics/Contexts/TextContext.cs
@@ -1,56 +1,55 @@
 ﻿using Annex.Core.Data;
 
-namespace Annex.Core.Graphics.Contexts
+namespace Annex.Core.Graphics.Contexts;
+
+public enum HorizontalAlignment
 {
-    public enum HorizontalAlignment
+    Left,
+    Center,
+    Right,
+}
+
+public enum VerticalAlignment
+{
+    Top,
+    Middle,
+    Bottom,
+}
+
+public class TextContext : DrawContext
+{
+    public IShared<float>? SuperSampleCount { get; init; }
+
+    public IShared<string> Text { get; }
+    public IShared<string> Font { get; }
+
+    public IVector2<float>? Position { get; init; }
+    public IVector2<float>? PositionOffset { get; init; }
+
+    public IShared<uint>? FontSize { get; set; }
+    public RGBA? Color { get; init; }
+
+    public IShared<float>? BorderThickness { get; init; }
+    public RGBA? BorderColor { get; init; }
+
+    public IShared<float>? Rotation { get; init; }
+    public HorizontalAlignment HorizontalAlignment { get; set; }
+    public VerticalAlignment VerticalAlignment { get; set; }
+
+    public TextContext(IShared<string> text, IShared<string> font)
     {
-        Left,
-        Center,
-        Right,
-    }
+        Text = text;
+        Font = font;
 
-    public enum VerticalAlignment
-    {
-        Top,
-        Middle,
-        Bottom,
-    }
+        Position = null;
+        PositionOffset = null;
+        FontSize = null;
+        Color = null;
+        BorderThickness = null;
+        BorderColor = null;
+        Rotation = null;
 
-    public class TextContext : DrawContext
-    {
-        public IShared<float>? SuperSampleCount { get; init; }
-
-        public IShared<string> Text { get; }
-        public IShared<string> Font { get; }
-
-        public IVector2<float>? Position { get; init; }
-        public IVector2<float>? PositionOffset { get; init; }
-
-        public IShared<uint>? FontSize { get; set; }
-        public RGBA? Color { get; init; }
-
-        public IShared<float>? BorderThickness { get; init; }
-        public RGBA? BorderColor { get; init; }
-
-        public IShared<float>? Rotation { get; init; }
-        public HorizontalAlignment HorizontalAlignment { get; set; }
-        public VerticalAlignment VerticalAlignment { get; set; }
-
-        public TextContext(IShared<string> text, IShared<string> font)
-        {
-            this.Text = text;
-            this.Font = font;
-
-            this.Position = null;
-            this.PositionOffset = null;
-            this.FontSize = null;
-            this.Color = null;
-            this.BorderThickness = null;
-            this.BorderColor = null;
-            this.Rotation = null;
-
-            this.VerticalAlignment = VerticalAlignment.Top;
-            this.HorizontalAlignment = HorizontalAlignment.Left;
-        }
+        VerticalAlignment = VerticalAlignment.Top;
+        HorizontalAlignment = HorizontalAlignment.Left;
     }
 }
